4ce353f6091ac03c421522e0dc3e731365d85059
Dedmen Miller <dedmen@dedmen.de>
2023-12-10 19:14:09 +0100

    Add scrollbar to report UI

--- addons/main/ui/ReportScreen.hpp
+++ addons/main/ui/ReportScreen.hpp
@@ -14,7 +14,7 @@ class GVAR(ReportScreen) {
     class controls {
         class buttonClose: ctrlButton {
             idc = -1;
-            x = QUOTE(safezoneW  + safezoneX - 32 * GRID_W);
+            x = QUOTE(safezoneW + safezoneX - 32 * GRID_W);
             y = QUOTE(safezoneH + safezoneY - 9 * GRID_H);
             w = QUOTE(30 * GRID_W);
             h = QUOTE(7 * GRID_H);
@@ -25,16 +25,27 @@ class GVAR(ReportScreen) {
             onButtonClick = QUOTE(ctrlParent (_this select 0) closeDisplay 2);
         };
 
-        class loadoutInfo: RscStructuredText {
-            idc = IDC_report_textBox;
-            text = "";
-            //type = 13; //CT_STRUCTURED_TEXT;  // defined constant
-            x = QUOTE(safezoneX + (20 * GRID_W));
-            y = QUOTE(safezoneY + (5 * GRID_H));
-            w = QUOTE(160 * GRID_W);
+        class myControl: RscControlsGroup {
+            x = QUOTE(safezoneX + (70 * GRID_W));
+            y = QUOTE(safezoneY + (12 * GRID_H));
+            w = QUOTE(200 * GRID_W);
             h = QUOTE(safezoneH - (34 * GRID_H));
-            sizeEx = QUOTE(5 * GRID_H);
-            colorBackground[]={0,0,0,0.6};
+            sizeEx = 0.03;
+            colorBackground[]={1,0,0,0.6};
+            class Controls
+            {
+                class loadoutInfo: RscStructuredText {
+                    idc = IDC_report_textBox;
+                    text = "";
+                    //type = 13; //CT_STRUCTURED_TEXT;  // defined constant
+                    x = 0;
+                    y = 0;
+                    w = 2;
+                    h = 20;
+                    size = QUOTE(4 * GRID_H);
+                    colorBackground[]={0,0,0,0.6};
+                };
+            };
         };
     };
 
